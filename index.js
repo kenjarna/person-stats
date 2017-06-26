746e2c81a651e89b78c6351ac9a62ad35b799be8
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-06-26 16:04:53 -0400

    Change heading when the button is clicked

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeHeading() {
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Ye Olde Person Stats'
+}
+const btn = document.querySelector('button')
+btn.addEventListener('click', changeHeading)
