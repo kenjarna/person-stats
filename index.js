dbabcb8234d4aedaf6d22f307e7858dcc8106324
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-06-26 22:32:00 -0400

    Add an additional input and add it next to the name below the form

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function changeHeading(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
+    const age = f.age.value
     const paragraph = document.querySelector('#pstats')
-    paragraph.textContent = name
+    paragraph.textContent = name + ', ' + age
 }
+
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
