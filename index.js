25c0dd8165911ca4d7ca230ae351fe47ae8633b6
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-06-26 23:18:15 -0400

    Change color of output (name & age) based on user input

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 function changeHeading(ev) {
+    //dont let the page refresh before we can see the changes!!
     ev.preventDefault()
     const f = ev.target
+
+    //Grab the string submitted
     const name = f.personName.value
+
+    //Grab the age submitted
     const age = f.age.value
+
+    //Grab the color submitted (if any)
+    const color = f.color.value
+
     const paragraph = document.querySelector('#pstats')
     paragraph.textContent = name + ', ' + age
+
+    paragraph.style.color = color
+    paragraph.style.fontSize = "X-Large"
+    paragraph.style.fontWeight = "bold"
 }
 
+function changetextcolor(ev) {
+    ev.preventDefault()
+    const f = ev.target
+
+
+}
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
