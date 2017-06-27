1cdb4919936643f54987e2952f43335d4077614e
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-06-26 22:23:12 -0400

    Add the name submitted to a paragraph at the end of the form

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function changeHeading(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const heading = document.querySelector('h1')
-    heading.textContent = name
+    const paragraph = document.querySelector('#pstats')
+    paragraph.textContent = name
 }
+
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
